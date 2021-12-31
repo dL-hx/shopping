b03ca5a0cb493c99204a675f547b744cd99718ba
dL-hx <897057246@qq.com>
2022-01-01 02:24:49 +0800

    1. 项目初始化完成

---
+++ code/src/components/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import CartTable from './CartTable';
+import ProductList from './ProductList';
+
+
+function App() {
+  return (
+    <div>
+       <ProductList />
+       <CartTable />
+    </div>
+  );
+}
+
+export default App;
